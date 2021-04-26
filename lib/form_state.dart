10e270530420724a9bd998d9793c0499cd0e3ab9
Olaleye Afolabi <olaleyeone@gmail.com>
2021-04-26 10:02:32 +0100

    exporting member and state classes

--- lib/form_state.dart
+++ lib/form_state.dart
@@ -1,3 +1,6 @@
+export 'src/form_member.dart';
+export 'src/form_member_state.dart';
 export 'src/form_group.dart';
+export 'src/form_group_state.dart';
 export 'src/form_control.dart';
 export 'src/form_control_state.dart';
